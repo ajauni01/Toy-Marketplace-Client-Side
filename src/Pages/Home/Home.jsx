1d776840c8912210c5a2464408fbe9788ac14a98
AJHARUL ISLAM AUNIK <AJAUNI01@WSC.EDU>
2023-05-22 19:59:00 -0500

    carousel-added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,39 @@
 
 
 const Home = () => {
+
+
   return (
     <div>
-      <p>Home Page</p>
 
+      <div className="carousel rounded-box  w-full h-full md:w-[1200px] md:h-[500px] mx-auto">
+        <div className="carousel-item w-1/2">
+          <img src="https://images.pexels.com/photos/15057238/pexels-photo-15057238/free-photo-of-baby-playing-with-vintage-toy-car.jpeg?auto=compress&cs=tinysrgb&w=600" className="w-full" />
+        </div>
+
+        <div className="carousel-item w-1/2">
+          <img src="https://images.pexels.com/photos/6461967/pexels-photo-6461967.jpeg?auto=compress&cs=tinysrgb&w=600" className="w-full" />
+        </div>
+
+        <div className="carousel-item w-1/2">
+          <img src="https://images.pexels.com/photos/6130568/pexels-photo-6130568.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Burger" className="w-full" />
+        </div>
+        <div className="carousel-item w-1/2">
+          <img src="https://images.pexels.com/photos/5501994/pexels-photo-5501994.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Burger" className="w-full" />
+        </div>
+
+        <div className="carousel-item w-1/2">
+          <img src="https://images.pexels.com/photos/10212593/pexels-photo-10212593.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Burger" className="w-full" />
+        </div>
+
+        <div className="carousel-item w-1/2">
+          <img src="https://images.pexels.com/photos/10212593/pexels-photo-10212593.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Burger" className="w-full" />
+        </div>
+
+        <div className="carousel-item w-1/2">
+          <img src="https://images.pexels.com/photos/7983790/pexels-photo-7983790.jpeg?auto=compress&cs=tinysrgb&w=600" className="w-full" />
+        </div>
+      </div>
     </div>
   );
 };
