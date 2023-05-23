81039dc05e88f275d71567f586ca4e594ef36779
AJHARUL ISLAM AUNIK <AJAUNI01@WSC.EDU>
2023-05-22 20:23:55 -0500

    banner-added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-
+import banner from '../../../public/banner.jpeg'
 
 const Home = () => {
 
@@ -6,6 +6,24 @@ const Home = () => {
   return (
     <div>
 
+      {/* banner section */}
+      <div className='max-w-7xl mx-auto '>
+        <img src={banner} alt="" />
+      </div>
+
+      {/* Featured Toys */}
+      <div className="stack mb-5 w-full ">
+        <div className="card shadow-md bg-blue-500 text-primary-content max-w-7xl mx-auto ">
+          <div className="card-body text-center">
+            <h2 className="font-bold text-3xl">Featured Toys</h2>
+            <p className="text-lg">
+              Discover the Perfect Playmate: Explore Our Featured Toys for Endless Fun!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* gallery section */}
       <div className="carousel rounded-box  w-full h-full md:w-[1200px] md:h-[500px] mx-auto">
         <div className="carousel-item w-1/2">
           <img src="https://images.pexels.com/photos/15057238/pexels-photo-15057238/free-photo-of-baby-playing-with-vintage-toy-car.jpeg?auto=compress&cs=tinysrgb&w=600" className="w-full" />
@@ -34,6 +52,7 @@ const Home = () => {
           <img src="https://images.pexels.com/photos/7983790/pexels-photo-7983790.jpeg?auto=compress&cs=tinysrgb&w=600" className="w-full" />
         </div>
       </div>
+      {/* gallery-section-ends */}
     </div>
   );
 };
