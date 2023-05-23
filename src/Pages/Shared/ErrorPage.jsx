034c8b0a966c2cd06cdc4593b6139073985301e0
AJHARUL ISLAM AUNIK <AJAUNI01@WSC.EDU>
2023-05-22 19:25:27 -0500

    404-page-added

--- src/Pages/Shared/ErrorPage.jsx
+++ src/Pages/Shared/ErrorPage.jsx
@@ -1,7 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import errorImg from '../../../public/error-page.jpg';
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  // function to navigate the user to the home page
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
   return (
-    <div>
-      <p>Page Not Found</p>
+    <div className="flex justify-center items-center h-screen">
+      <div className="max-w-7xl">
+        <div>
+          <img src={errorImg} alt="" />
+        </div>
+
+        <button className="btn btn-primary text-center" onClick={handleGoHome}>
+          Go Home
+        </button>
+      </div>
     </div>
   );
 };
